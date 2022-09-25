a728cd7e002d30901464d2acd09951745c48d6c5
Marco Wang <m.aesophor@gmail.com>
2022-09-25 11:13:33 +0800

    SDK: Add HitGroups
    Signed-off-by: Marco Wang <m.aesophor@gmail.com>

---
+++ src/SDK/HitGroups.h
@@ -0,0 +1,39 @@
+// Copyright (c) 2022 Marco Wang <m.aesophor@gmail.com>. All rights reserved.
+#ifndef HAVOC_HIT_GROUPS_H_
+#define HAVOC_HIT_GROUPS_H_
+
+enum class Bone : int {
+  INVALID = -1,
+  PELVIS = 0,
+  LEAN_ROOT,
+  CAM_DRIVER,
+  HIP,
+  LOWER_SPINAL_COLUMN,
+  MIDDLE_SPINAL_COLUMN,
+  UPPER_SPINAL_COLUMN,
+  NECK,
+  HEAD,
+};
+
+enum class Hitbox : int {
+  HEAD = 0,
+  NECK,
+  PELVIS,
+  SPINE,
+  LEGS,
+  ARMS,
+};
+
+enum class HitGroups : int {
+  GENERIC = 0,
+  HEAD,
+  CHEST,
+  STOMACH,
+  LEFTARM,
+  RIGHTARM,
+  LEFTLEG,
+  RIGHTLEG,
+  GEAR
+};
+
+#endif  // HAVOC_HIT_GROUPS_H_
